21cae758735575b50229c2863ba0fd8942f9e974
Matt Myers <myersmt@pm.me>
2023-02-09 18:13:07 -0600

    update

--- Py/hw002_1.py
+++ Py/hw002_1.py
@@ -45,18 +45,99 @@ for file in os.listdir(os.getcwd()):
 #     print(f'\n{key}\n{val.head()}')
 
 # Organize the data
+def sortData(D, Xi):
+    return D[[Xi,'y_n']].sort_values(by=Xi).reset_index(drop=True)
 
 # Determine splits
 def DetermineCandidateSplits(D, Xi):
     C=[] # initialize set of candidate splits for feature Xi
-    data_sort = D[[Xi, 'y_n']].sort_values(by=Xi).reset_index(drop=True)
+    data_sort = sortData(D,Xi)
     # print(data_sort)
     for j in range(len(D)-1):
         if data_sort['y_n'][j] != data_sort['y_n'][j+1]:
-            C.append(D[Xi][j])
+            C.append(data_sort[Xi][j])
     return(C)
 
-print(len(DetermineCandidateSplits(data_raw['D1.txt'],'x_n2')))
+#print(DetermineCandidateSplits(data_raw['D1.txt'],'x_n1'))
+#print(sortData(data_raw['D1.txt'],'x_n1'))
+
+# Entropy
+def entropy(Y):
+    """
+    Also known as Shanon Entropy
+    Reference: https://en.wikipedia.org/wiki/Entropy_(information_theory)
+    """
+    unique, count = np.unique(Y, return_counts=True, axis=0)
+    prob = count/len(Y)
+    en = np.sum((-1)*prob*np.log2(prob))
+    return en
+
+
+# Joint Entropy
+def jEntropy(Y,S):
+    """
+    H(Y;X)
+    Reference: https://en.wikipedia.org/wiki/Joint_entropy
+    """
+    YS = np.c_[Y,S]
+    return entropy(YS)
+
+# Conditional Entropy
+def cEntropy(Y, S):
+    """
+    conditional entropy = Joint Entropy - Entropy of X
+    H(Y|X) = H(Y;X) - H(X)
+    Reference: https://en.wikipedia.org/wiki/Conditional_entropy
+    """
+    return jEntropy(Y, S) - entropy(S)
+
+
+# Information Gain
+def InfoGain(Y, S):
+    """
+    Information Gain, I(Y;X) = H(Y) - H(Y|X)
+    Reference: https://en.wikipedia.org/wiki/Information_gain_in_decision_trees#Formal_definition
+    """
+    return entropy(Y) - cEntropy(Y,S)
+
+# Gain Ratio
+def GainRatio(D,S):
+    return InfoGain(D,S)/entropy(S)
+
+def Success(D, splitVal):
+    Xi = list(D)[0]
+    beforeCount={1:0,0:0}
+    afterCount={1:0,0:0}
+    for i, d in enumerate(D[Xi]):
+        if d < splitVal:
+            if D['y_n'][i] == 0:
+                beforeCount[0]+=1
+            if D['y_n'][i] == 1:
+                beforeCount[1]+=1
+        elif d > splitVal:
+            if D['y_n'][i] == 0:
+                afterCount[1]+=1
+            if D['y_n'][i] == 1:
+                afterCount[0]+=1
+    #print(beforeCount, afterCount)
+    return beforeCount, afterCount
+
+#Success(sortData(data_raw['D1.txt'],'x_n1'),0.5)
+
+def SplitGain(D, Xi):
+    gains = {}
+    splits = DetermineCandidateSplits(D,Xi)
+    dsort = sortData(D,Xi)
+    for d in dsort[Xi]:
+        if d in splits:
+            gains.update({d:Success(D,d)})
+    return(gains)
+
+# SplitGain(data_raw['D1.txt'],'x_n1')
+dict_of_gains=SplitGain(data_raw['D1.txt'],'x_n1')
+print(f'Key\t\tBefore:\t\tAfter:')
+for key,val in dict_of_gains.items():
+    print(key, val)
 
 # find the best splits
 def FindBestSplits(D,C):
@@ -72,7 +153,7 @@ def MakeSubtree(D):
     # else:
     #     make internal node N
     #     S = FindBestSplits(D,C)
-    #     for outcomeK in S:
+    #     for; outcomeK in S:
     #         Dk = subset of instances that have outcome k
     #         kth child of N = MakeSubtree(Dk)
     # return(subtree@N)
