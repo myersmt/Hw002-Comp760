b699d5dde68776471c4c28f538031e5d84bd6353
Matt Myers <myersmt@pm.me>
2023-02-15 03:49:02 -0600

    update_big

---
+++ Py/q4/Hw_002-Q4.py
@@ -0,0 +1,14 @@
+"""
+Created by: Matt Myers
+02/15/2023
+
+Q4
+"""
+import sklearn
+from sklearn import tree
+import numpy as np
+import pandas as pd
+import os
+import graphviz
+from matplotlib import pyplot as plt
+
