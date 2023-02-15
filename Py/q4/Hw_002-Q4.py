f6fbe83a73f8ac3fcfec8b1fbe1aea75e2ed8328
Matt Myers <myersmt@pm.me>
2023-02-15 04:46:43 -0600

    update_big

--- Py/q4/Hw_002-Q4.py
+++ Py/q4/Hw_002-Q4.py
@@ -11,4 +11,31 @@ import pandas as pd
 import os
 import graphviz
 from matplotlib import pyplot as plt
+from scipy.interpolate import lagrange
+from numpy.polynomial.polynomial import Polynomial
 
+step_num= 50
+min = -np.pi
+max = np.pi
+
+train_set = np.array(sorted(np.random.uniform(min, max, step_num)))
+train_y = np.sin(train_set)
+poly = lagrange(train_set, train_y)
+
+test_set = np.array(sorted(np.random.uniform(min, max, step_num)))
+plt.scatter(train_set,poly(train_set),label='Train')
+plt.scatter(test_set, poly(test_set), label='Test: Polynomial')
+plt.plot(train_set, train_y, label=r"Sin(X)", linestyle='-.')
+plt.legend()
+plt.axis([-2,2,-2,2])
+plt.show()
+
+def error(set):
+    return sum(np.abs((poly(set)-np.sin(set))))/len(set)
+
+train_error = error(train_set)
+test_error = error(test_set)
+print(train_error, test_error)
+
+# plt.plot(x,fun_y)
+# plt.show()
